b5ed8369267f8f3002ef4ac6ffb5b69976445594
Jane Hu <30421010+dracarystargaryen@users.noreply.github.com>
2022-08-10 16:22:39 -0400

    Update PathFinderAlgo.h

--- Pathfinding/PathFinderAlgo.h
+++ Pathfinding/PathFinderAlgo.h
@@ -4,11 +4,12 @@ namespace pf {
 	class PathFinderAlgo {
 	public:
 		inline PathFinderAlgo(int i, int j) : i(i), j(j) {}
-		inline PathFinderAlgo() = default;
+		inline PathFinderAlgo() = default; //Pathfinding algorithm declared
 
-		inline bool operator == (const PathFinderAlgo& v) const { return (i == v.i) && (j == v.j); }
+		inline bool operator == (const PathFinderAlgo& v) const { return (i == v.i) && (j == v.j); } //operator passing through each function
 		inline const PathFinderAlgo operator+ (const PathFinderAlgo& v) const { return PathFinderAlgo(i + v.i, j + v.j); }
-
+                
+		//Delta calls for the start time of when the algorithm begins 
 		static PathFinderAlgo getDelta(const PathFinderAlgo v1, const PathFinderAlgo v2) { return PathFinderAlgo(abs(v1.i - v2.j), abs(v1.j - v2.j)); }
 
 		int i;
