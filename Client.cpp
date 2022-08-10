5280197669b6ee9ab8ddb7ae8f4f2ab1640b615c
Jane Hu <30421010+dracarystargaryen@users.noreply.github.com>
2022-08-10 12:41:30 -0400

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <signal.h>
+#include <mutex>
+#define MAX_LEN 200
+
+
+using namespace std;
+
+bool exit_flag = false;
+thread t_send, t_recv;
+int client_socket;
+
+void catch_ctrl_c(int signal);
+int eraseText(int cnt);
+void send_message(int client_socket);
+void recv_message(int client_socket);
+
+int main()
+{
+    if((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket: ");
+        exit(-1);
+    }
+    struct socaddr_in client;
+    client.sin_family = AF_INET;
+    client.sin_port = htons(10000);
+    client.sin_addr.s_addr = INADDR_ANY;
+    bzero(&client.sin_zero, 0);
+
+    if((connect(client_socket,(struct sockaddr *)&client, sizeof(struct sockaddr_in))) == -1)
+    {
+        perror("connect: ");
+        exit(-1);
+    }
+
+    signal(SIGNIT, catch_ctrl_c);
+    char name(MAX_LEN);
+    cout << "Enter your name: ";
+    cin.getline(name, MAX_LEN);
+    send(client_socket, name, sizeof(name), 0);
+
+    thread thr1(send_message, client_socket);
+    thread thr2(recv_message, client_socket);
+
+    t_send = move(thr1);
+    t_recv = move(thr2);
+
+    if(t_send.joinable())
+        t_send.join();
+    if(t_recv.joinable())
+        r_recv.join();
+
+    return 0;
+
+}
+
+void catch_ctrl_c(int signal)
+{
+    char str[MAX_LEN] = "#exit";
+    send(client_socket, str, sizeof(str), 0);
+    exit_flag = true;
+    t_send.detach();
+    t_recv.detach();
+    close(client_socket);
+    exit(signal);
+}
+
+int textErase(int cnt)
+{
+    char backspace = 0;
+    for (int x = 0; x < cnt; x++)
+    {
+        cout << backspace;
+    }
+}
+
+void send_message(int client_socket)
+{
+    while(1)
+    {
+        cout << "You :" << endl;
+        char str[MAX_LEN];
+        cin.getline(str, MAX_LINE);
+        send(client_socket, str, sizeof(str), 0);
+        if(strcmp(str, "#exit") == 0)
+        {
+            exit_flat = true;
+            t_recv.detach();
+            close(client_socket);
+            return;
+        }
+    }
+}
+
+void recv_message(int client_socket)
+{
+    while(1)
+    {
+        if(exit_flag)
+            return;
+        char name[MAX_LEN], str[MAX_LEN];
+        int bytes_received = recv(client_socket, name, sizeof(name),0);
+        if(bytes_received <= 0)
+            continue;
+        recv(client_socket, str, sizeof(str), 0);
+        textErase(6);
+        if(strcmp(name, "#NULL") != 0)
+            cout << name << "You: " << endl;
+        else
+            cout << str << endl;
+        cout << "You: " << endl;
+        fflush(stdout);
+    }
+}
---
+++ Server.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#define MAX_LEN 200
+
+using namespace std;
+
+struct terminal
+{
+    int id;
+    string user;
+    int socket;
+    thread thr;
+}
+
+vector<terminal> clients;
+int seed = 0;
+mutex cout_mtx, clients_mtx;
+
+void set_name(int id, char user[]);
+void shared_print(string str, bool endLine);
+int broadcast_message(string message, int sender_id);
+int broadcast_message(int num, int sender_id);
+void end_connection(int id);
+void handle_client(int client_socket, int id);
+
+int main(){
+    int server_socket;
+    if((server_socket=socket(AF_INET, SOCK_STREAM,0))==-1)
+    {
+        perror("socket: ");
+        exit(-1);
+    }
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(10000);
+    server.sin_addr.s_addr = INADDR_ANY;
+    bzero(&server.sin_zero, 0);
+
+    if((bind(server_socket, (struct sockaddr *)&server, sizeof(struct sockaddr_in))) == -1)
+    {
+        perror("bind error: ");
+        exit(-1);
+    }
+
+    if((listen(server_socket)) == -1)
+    {
+        perror("listen error: ");
+        exit(-1);
+    }
+
+    struct sockaddr_in client;
+    int client_socket;
+    unsigned int len = sizeof(sockaddr_in);
+
+    cout << "Network chat" << endl;
+
+    while (1)
+    {
+        if ((client_socket = accept(server_socket,(struct sockaddr *)&client, &len)) == -1)
+        {
+            perror("accept error: ");
+            exit(-1);
+        }
+        seed++;
+        thread thr(handle_client, client_socket, seed);
+        lock_guard<mutex> guard(clients_mtx);
+        clients.push_back({seed, stirng("Anonymous"), client_socket,(move(t))});
+    }
+
+    for (int x = 0; x < clients.size(); x++)
+    {
+        if (clients[x].thr.joinable())
+            clients[x].thr.join();
+    }
+    close(server_socket);
+    return 0;
+}
+
+void set_name(int id, char user[])
+{
+    for (int x = 0; x < clietns.size(); x++)
+    {
+        if(clients[x].id == id)
+        {
+            clients[x].name = string(name);
+        }
+    }
+}
+
+void shared_print(string str, bool endLine = true)
+{
+    lock_guard<mutex> guard(cout_mtx);
+    cout << str;
+    if(endLine)
+        cout << endl;
+}
+
+int broadcast_message(string message, int sender_id)
+{
+    char temp[MAX_LEN];
+    strcpy(temp, message.c_str());
+    for (int x = 0; x < clients.size(); x++)
+    {
+        if(clients[x].id != sender_id)
+        {
+            send(clients[x].socket, &num, sizeof(num),0);
+        }
+    }
+}
+
+void end_connection(int id)
+{
+    for (int x = 0; x < clients.size(); x++)
+    {
+        if (clients[x].id == id)
+        {
+            lock_guard<mutex> guard(clients.mtx);
+            clients[x].thr.detach();
+            clients.erase(clients.begin() + x);
+            close(clients[x].socket);
+            break;
+        }
+    }
+}
+
+void handle_client(int client_socket, int id)
+{
+    char name[MAX_LEN], str[MAX_LEN];
+    recv(client_socket, name, sizeof(name), 0);
+    set_name(id, name);
+    string welcome_message = string(name) + string(" has joined the chat.");
+    broadcast_message("#NULL", id);
+    broadcast_message(id, id);
+    broadcast_message(welcome_message,id);
+    
+    while (1)
+    {
+        int bytes_received = recv(client_socket, str, sizeof(str), 0);
+        if(bytes_received <= 0)
+            return;
+        if(strcmp(str, "#exit") == 0)
+        {
+            string message = string(name) + string(" has left");
+            broadcast_message("#NULL", id);
+            broadcast_message(id, id);
+            broadcast_message(message, id);
+            end_connection(id);
+            return;
+        }
+        broadcast_message(string(name),id);
+        broadcast_message(id, id);
+        broadcast_message(string(str),id);
+    }
+
+}
